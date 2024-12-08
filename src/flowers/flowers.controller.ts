ed98d9affd2bcce3307194bf420a51666d54fb17
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-08 14:17:20 +0300

    Adds pagination support to flowers endpoint
    Introduces a `pageNumber` query parameter to the `findAll` method in the `FlowersController`.  This allows clients to specify the desired page number for retrieving paginated flower data.
    
    A parse int pipe is used to ensure the `pageNumber` parameter is correctly interpreted as a number.  Console logging of the parameter is added for debugging purposes.

--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
+import { ParseIntPipe } from 'src/pipes/pipe';
 
 @Controller('flowers')
 export class FlowersController {
   constructor(private readonly flowersService: FlowersService) {}
+
   @Get()
-  findAll() {
+  findAll(@Query('pageNumber', ParseIntPipe) pageNumber: number) {
+    console.log(pageNumber);
     return this.flowersService.findAll();
   }
 }
