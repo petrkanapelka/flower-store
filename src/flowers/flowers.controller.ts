d7ed90016a679a46caa4c24cccfd4ad75f81b310
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-10 15:13:35 +0300

    Adds auth guard and logging interceptor to FlowersController
    Protects the `findAll` endpoint with the `AuthGuard` and adds a `LoggingInterceptor` to the controller.

--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FlowersService } from './flowers.service';
 import { ParseIntPipe } from 'src/pipes/pipe';
+import { AuthGuard } from 'src/guard/guard';
+import { LoggingInterceptor } from 'src/interceptors/interceptor';
 
 @Controller('flowers')
+@UseInterceptors(LoggingInterceptor)
 export class FlowersController {
   constructor(private readonly flowersService: FlowersService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll(@Query('pageNumber', ParseIntPipe) pageNumber: number) {
     console.log(pageNumber);
     return this.flowersService.findAll();
